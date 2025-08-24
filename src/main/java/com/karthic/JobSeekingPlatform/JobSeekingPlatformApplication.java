8b631579c715f8c9dcabf7bf64edb07572a672f4
Karthic <karthics004@gmail.com>
2025-08-24 13:59:23 +0530

    Initial commit

---
+++ src/main/java/com/karthic/JobSeekingPlatform/JobSeekingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.karthic.JobSeekingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthic/JobSeekingPlatform/JobSeekingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthic.JobSeekingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
