9c210a79c5b3035f3114432c199c1aa31e3fd75e
Karthic <karthics004@gmail.com>
2025-08-30 08:59:21 +0530

    Added GlobalExceptionHandler

---
+++ src/main/java/com/karthic/JobSeekingPlatform/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.karthic.JobSeekingPlatform.Exception;
+
+
+import com.karthic.JobSeekingPlatform.payload.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> CustomMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String,String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err ->{
+        String fieldName = ((FieldError)err).getField();
+        String message = err.getDefaultMessage();
+        response.put(fieldName,message);
+
+        });
+
+        return new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<APIResponse> CustomResourceNotFoundException(ResourceNotFoundException e){
+        String message = e.getMessage();
+        APIResponse apiResponse = new APIResponse(message,false );
+        return new ResponseEntity<>(apiResponse,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<APIResponse> CustomAPIException(APIException e){
+        String message = e.getMessage();
+        APIResponse apiResponse = new APIResponse(message,false );
+        return new ResponseEntity<>(apiResponse,HttpStatus.BAD_REQUEST);
+    }
+
+}
