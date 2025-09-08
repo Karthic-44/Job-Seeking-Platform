7719c5d461313ebdacb71b7c3373f649635f3554
Karthic <karthics004@gmail.com>
2025-09-08 22:52:13 +0530

    Added JobController

---
+++ src/main/java/com/karthic/JobSeekingPlatform/controller/JobController.java
@@ -0,0 +1,34 @@
+package com.karthic.JobSeekingPlatform.controller;
+
+import com.karthic.JobSeekingPlatform.payload.JobDTO;
+import com.karthic.JobSeekingPlatform.service.JobPostingService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+public class JobController {
+
+    @Autowired
+    private JobPostingService jobPostingService;
+
+
+    @PostMapping("/public/job")
+    public ResponseEntity<JobDTO> CreateCategories(@Valid @RequestBody JobDTO jobDTO) {
+
+        JobDTO savedJobDTO = jobPostingService.createJob(jobDTO);
+        return new ResponseEntity<>(savedJobDTO, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/admin/job/{jobId}")
+    public ResponseEntity<JobDTO> deleteCategory(@PathVariable Long jobId){
+
+        JobDTO deleteJob = jobPostingService.deleteJob(jobId);
+        return new ResponseEntity<>(deleteJob, HttpStatus.OK);
+    }
+
+}
