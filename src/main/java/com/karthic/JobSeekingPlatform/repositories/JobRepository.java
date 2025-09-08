45339a6ec945481828a9d695cafd398e32cefd47
Karthic <karthics004@gmail.com>
2025-09-08 22:51:44 +0530

    updated JobRepository

--- src/main/java/com/karthic/JobSeekingPlatform/repositories/JobRepository.java
+++ src/main/java/com/karthic/JobSeekingPlatform/repositories/JobRepository.java
@@ -1,7 +1,11 @@
 package com.karthic.JobSeekingPlatform.repositories;
 
 import com.karthic.JobSeekingPlatform.model.Job;
+import com.karthic.JobSeekingPlatform.payload.JobDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job,Long> {
+
+    Job findByJobName(String jobName);
+
 }
