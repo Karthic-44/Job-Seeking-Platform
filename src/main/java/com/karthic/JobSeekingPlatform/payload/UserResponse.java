075fa6b3ab5f97036f4c8b707b820c61a930e1cf
Karthic <karthics004@gmail.com>
2025-12-22 17:45:51 +0530

    Added userResponse

---
+++ src/main/java/com/karthic/JobSeekingPlatform/payload/UserResponse.java
@@ -0,0 +1,21 @@
+package com.karthic.JobSeekingPlatform.payload;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private List<UsersDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
+
+}
