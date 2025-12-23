7b54084b82af6a9ed51f1c9a36f5c911a2089930
Karthic <karthics004@gmail.com>
2025-12-23 08:25:28 +0530

    Added ExperienceResponse

---
+++ src/main/java/com/karthic/JobSeekingPlatform/payload/ExperienceResponse.java
@@ -0,0 +1,20 @@
+package com.karthic.JobSeekingPlatform.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExperienceResponse {
+    private List<ExperienceDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
+}
+
