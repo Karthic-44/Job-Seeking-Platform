48b83db8ead4a56ed0cb65fe62e9590490759787
Karthic <karthics004@gmail.com>
2025-12-27 21:49:32 +0530

    Added ApplicationService

---
+++ src/main/java/com/karthic/JobSeekingPlatform/service/ApplicationService.java
@@ -0,0 +1,10 @@
+package com.karthic.JobSeekingPlatform.service;
+
+import com.karthic.JobSeekingPlatform.payload.ApplicationDTO;
+
+
+public interface ApplicationService {
+
+    public ApplicationDTO createApplication(ApplicationDTO applicationDTO);
+    
+}
