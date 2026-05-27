4eec468cad557b42bb92aa71eb709937ffe8947e
Karthic <karthics004@gmail.com>
2026-05-27 21:50:30 +0530

    updated react+vite and added tailwindCSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+         <h1 className="text-blue-500 text-4xl font-bold">
+      Hello world
+    </h1>
     </>
   )
 }
